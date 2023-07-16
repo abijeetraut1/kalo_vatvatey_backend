7afae157f0263f8f847971473bddbea63ff47fa8
Abijeet <63050833+AbijeetRaut@users.noreply.github.com>
2023-07-16 14:15:53 +0545

    Update app.js

--- app.js
+++ app.js
@@ -21,103 +21,98 @@ app.use(express.urlencoded({
 }));
 
 // if(process.env.enviroment === "development"){
+// const session = require('express-session');
+// app.use(session({
+//     secret: process.env.session_secret,
+//     resave: true,
+//     saveUninitialized: true
+// }));
 
-    
-    const session = require('express-session');
-    app.use(session({
-        secret: process.env.session_secret,
-        resave: true,
-        saveUninitialized: true
-    }));
-    
-    app.use(passport.initialize())
-    app.use(passport.session())
-    
-    
-    //Google Authentication
-    passport.serializeUser(function (user, cb) {
-        cb(null, user);
-    });
-    
-    passport.deserializeUser(function (obj, cb) {
-        cb(null, obj);
-    });
-    
-    /*  Google AUTH  */
-    
-    var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
-    
-    passport.use(
-        new GoogleStrategy({
-                clientID: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
-                callbackURL: "http://localhost:8000/auth/google/callback",
-            },
-            function (accessToken, refreshToken, profile, done) {
-                userProfile = profile;
-                return done(null, userProfile);
-            }
-        )
-    );
-        
-    app.get(
-        "/auth/google",
-        passport.authenticate("google", {
-            scope: ["profile", "email"]
-        })
-    );
-    
-    app.get(
-        "/auth/google/callback",
-        passport.authenticate("google", {
-            failureRedirect: "http://127.0.0.1:3000/login",
-        }),
-        async function (req, res) {
-            const findUserByEmail = await db.users.findAll({
-                where: {
-                
-                    email: userProfile.emails[0].value,
-                }
-            });
-            let token;
-        
-        
-        
-            if (findUserByEmail.length > 0) {
-                token = jwt.sign({
-                    id: findUserByEmail[0].id
-                }, process.env.JWT_SECRET, {
-                    expiresIn: process.env.COOKIE_EXPIRES_IN
-                })
-            } else {
-                let user
-                try {
-                    user = await db.users.create({
-                        firstName: userProfile.name.givenName,
-                        lastName: userProfile.name.familyName,
-                        email: userProfile.emails[0].value,
-                        password: await bcrypt.hash(userProfile.id + Date.now(), 12),
-                        role: "user"
-                    });
-                    token = jwt.sign({
-                        id: userProfile.id
-                    }, process.env.JWT_SECRET, {
-                        expiresIn: process.env.COOKIE_EXPIRES_IN
-                    })
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-            res.cookie("jwt", token, {
-                httpOnly: true
-            })
-        
-            // Successful authentication, redirect success.
-            res.redirect("http://localhost:3000/" + token)
-        
-        }
-    );
+// app.use(passport.initialize())
+// app.use(passport.session())
 
+
+// //Google Authentication
+// passport.serializeUser(function (user, cb) {
+//     cb(null, user);
+// });
+
+// passport.deserializeUser(function (obj, cb) {
+//     cb(null, obj);
+// });
+
+// /*  Google AUTH  */
+
+// var GoogleStrategy = require("passport-google-oauth").OAuth2Strategy;
+
+// passport.use(
+//     new GoogleStrategy({
+//             clientID: process.env.CLIENT_ID,
+//             clientSecret: process.env.CLIENT_SECRET,
+//             callbackURL: "http://localhost:8000/auth/google/callback",
+//         },
+//         function (accessToken, refreshToken, profile, done) {
+//             userProfile = profile;
+//             return done(null, userProfile);
+//         }
+//     )
+// );
+
+// app.get(
+//     "/auth/google",
+//     passport.authenticate("google", {
+//         scope: ["profile", "email"]
+//     })
+// );
+
+// app.get(
+//     "/auth/google/callback",
+//     passport.authenticate("google", {
+//         failureRedirect: "http://127.0.0.1:3000/login",
+//     }),
+//     async function (req, res) {
+//         const findUserByEmail = await db.users.findAll({
+//             where: {
+
+//                 email: userProfile.emails[0].value,
+//             }
+//         });
+//         let token;
+
+//         if (findUserByEmail.length > 0) {
+//             token = jwt.sign({
+//                 id: findUserByEmail[0].id
+//             }, process.env.JWT_SECRET, {
+//                 expiresIn: process.env.COOKIE_EXPIRES_IN
+//             })
+//         } else {
+//             let user
+//             try {
+//                 user = await db.users.create({
+//                     firstName: userProfile.name.givenName,
+//                     lastName: userProfile.name.familyName,
+//                     email: userProfile.emails[0].value,
+//                     password: await bcrypt.hash(userProfile.id + Date.now(), 12),
+//                     role: "user"
+//                 });
+//                 token = jwt.sign({
+//                     id: userProfile.id
+//                 }, process.env.JWT_SECRET, {
+//                     expiresIn: process.env.COOKIE_EXPIRES_IN
+//                 })
+//             } catch (error) {
+//                 console.log(error)
+//             }
+//         }
+//         res.cookie("jwt", token, {
+//             httpOnly: true
+//         })
+
+//         // Successful authentication, redirect success.
+//         res.redirect("http://localhost:3000/" + token)
+
+//     }
+// );
 // }
 
 
