7e90e833ab413350b5d22f8d842186f6af162a6c
Abijeet <63050833+AbijeetRaut@users.noreply.github.com>
2023-07-19 13:22:28 +0545

    Update globalErrorHandler.js

--- controller/globalErrorHandler.js
+++ controller/globalErrorHandler.js
@@ -2,7 +2,7 @@ const statusFunc = require("../utils/statusFunc")
 require('dotenv').config();
 
 module.exports = (err, req, res, next) => {
-    if (process.env.ENVIROMENT === "production") {
+    if (process.env.enviroment === "production") {
         if (err.name === "SyntaxError") {
             return statusFunc(res, 500, "SERVER IS UNDER MAINTAINENCE! PLEASE WAIT COUPLE OF MINUTES")
         }else if(err.name === 'JsonWebTokenError'){
@@ -13,7 +13,7 @@ module.exports = (err, req, res, next) => {
             return statusFunc(res, 400, `forgot to add field: ${err.errors[0].path}`)
         }
 
-    } else if (process.env.ENVIROMENT === "development") {
+    } else if (process.env.enviroment === "development") {
         if (
             err.name === "SyntaxError" ||
             err.name === 'JsonWebTokenError' ||
@@ -34,4 +34,5 @@ module.exports = (err, req, res, next) => {
         }
     }
     console.log("err.name")
+    next();
 }
